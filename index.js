345c1a04a190f352e3d22ce3561e9fa52624915a
rafi993 <rafi993@protonmail.com>
2019-10-10 21:58:43 +0530

    Updating cellular automata after rules

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const createGrid = ({ x, y }) =>
   [...Array(x)].fill(0).map(() =>
     Array(y)
       .fill(0)
-      .map(() => Math.floor(Math.random() * 100) + 1 > 98)
+      .map(() => (Math.floor(Math.random() * 100) + 1 > 10 ? 1 : 0))
   );
 
-const grid = createGrid({ x, y });
+let grid = createGrid({ x, y });
+let shadowGrid = JSON.parse(JSON.stringify(grid));
 
 //get DPI
 const dpi = window.devicePixelRatio;
@@ -42,16 +43,88 @@ const draw = () => {
 
   scaleCanvas();
 
-  grid.forEach((x, i) => {
-    x.forEach((y, j) => {
-      if (y) {
+  grid.forEach((y, j) => {
+    y.forEach((x, i) => {
+      if (x) {
         ctx.fillStyle = "white";
       } else {
         ctx.fillStyle = "black";
       }
-      ctx.fillRect(i * 5, j * 5, 150, 100);
+      ctx.fillRect(i * 5, j * 5, 100, 100);
     });
   });
 };
 
+const applyRules = () => {
+  console.log("HEllo");
+  grid.forEach((y, j) => {
+    y.forEach((x, i) => {
+      let livingNeighbours = 0;
+
+      // Top Row
+      {
+        if (j > 0) {
+          const topRow = grid[j - 1];
+
+          if (i > 0) {
+            livingNeighbours += topRow[i - 1]; // Top Left Cell
+          }
+
+          livingNeighbours += topRow[i]; // Top Center Cell
+
+          if (topRow.length < i + 1) {
+            livingNeighbours += topRow[i + 1];
+          }
+        }
+      }
+
+      // Current Row
+      {
+        const currentRow = grid[j];
+
+        if (i > 0) {
+          livingNeighbours += currentRow[i]; // Left Cell
+        }
+
+        if (currentRow.length < i + 1) {
+          livingNeighbours += currentRow[i + 1];
+        }
+      }
+
+      // Bottom Row
+      {
+        if (j + 1 < grid.length) {
+          const bottomRow = grid[j + 1];
+
+          if (i > 0) {
+            livingNeighbours += bottomRow[i - 1]; // Bottom Left Cell
+          }
+
+          livingNeighbours += bottomRow[i]; // Bottom Center Cell
+
+          if (bottomRow.length < i + 1) {
+            livingNeighbours += bottomRow[i + 1];
+          }
+        }
+      }
+
+      if (x === 0 && livingNeighbours === 3) {
+        shadowGrid[j][i] = 1; // Becomes alive
+      } else if (x === 1 && livingNeighbours < 2) {
+        shadowGrid[j][i] = 0; // Die of under population
+      } else if (x === 1 && livingNeighbours > 3) {
+        shadowGrid[j][i] = 0; // Die of over population
+      }
+    });
+  });
+
+  // Copy shadow grid to actual grid
+  grid = JSON.parse(JSON.stringify(shadowGrid));
+};
+
 draw();
+
+setInterval(() => {
+  applyRules();
+  draw();
+}, 500);
